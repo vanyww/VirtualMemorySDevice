438f95f070527e3c0af134bd912014970eac155b
Ivan Shestakov <vanyww@mail.ru>
2023-05-07 19:20:08 +0300

    Create and move SDevice configuration to separate header file

---
+++ Inc/VirtualMemorySDevice/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "SDeviceCore/core.h"
+
+#if (SDEVICE_CORE_VERSION_MAJOR != 8) || (SDEVICE_CORE_VERSION_MINOR < 0)
+#error SDevice core version is incorrect.
+#endif
+
+#ifdef VIRTUAL_MEMORY_SDEVICE_USE_EXTERNAL_CONFIG
+#include "virtual_memory_sdevice_config.h"
+#endif
+
+#ifndef VIRTUAL_MEMORY_SDEVICE_FILLER_DATA_VALUE
+#define VIRTUAL_MEMORY_SDEVICE_FILLER_DATA_VALUE 0x00
+#endif
--- Inc/VirtualMemorySDevice/public.h
+++ Inc/VirtualMemorySDevice/public.h
@@ -1,12 +1,8 @@
 #pragma once
 
-#include "SDeviceCore/core.h"
+#include "config.h"
 
 #include <stdbool.h>
-
-#if (SDEVICE_CORE_VERSION_MAJOR != 8) || (SDEVICE_CORE_VERSION_MINOR < 0)
-#error SDevice core version is incorrect.
-#endif
 
 #define VIRTUAL_MEMORY_SDEVICE_VERSION_MAJOR 2
 #define VIRTUAL_MEMORY_SDEVICE_VERSION_MINOR 0
