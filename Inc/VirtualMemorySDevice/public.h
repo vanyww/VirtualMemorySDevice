108c16119b3eb262eb3f03769d148c94b7a2a35c
Ivan Shestakov <vanyww@mail.ru>
2023-05-07 19:09:21 +0300

    Implement SDevice status and chunk status validation macroes

--- Inc/VirtualMemorySDevice/public.h
+++ Inc/VirtualMemorySDevice/public.h
@@ -29,12 +29,29 @@ typedef enum
    VIRTUAL_MEMORY_SDEVICE_STATUS_CHUNK_VALIDATION_FAIL
 } VirtualMemorySDeviceStatus;
 
+#define VIRTUAL_MEMORY_SDEVICE_IS_VALID_STATUS(value) (                                                                \
+{                                                                                                                      \
+   __auto_type _value = (value);                                                                                       \
+   _value == VIRTUAL_MEMORY_SDEVICE_STATUS_OK                    ||                                                    \
+   _value == VIRTUAL_MEMORY_SDEVICE_STATUS_WRONG_ADDRESS         ||                                                    \
+   _value == VIRTUAL_MEMORY_SDEVICE_STATUS_CHUNK_PROCESSING_FAIL ||                                                    \
+   _value == VIRTUAL_MEMORY_SDEVICE_STATUS_CHUNK_VALIDATION_FAIL;                                                      \
+})
+
 typedef enum
 {
    VIRTUAL_MEMORY_SDEVICE_CHUNK_STATUS_OK              = VIRTUAL_MEMORY_SDEVICE_STATUS_OK,
    VIRTUAL_MEMORY_SDEVICE_CHUNK_STATUS_PROCESSING_FAIL = VIRTUAL_MEMORY_SDEVICE_STATUS_CHUNK_PROCESSING_FAIL,
    VIRTUAL_MEMORY_SDEVICE_CHUNK_STATUS_VALIDATION_FAIL = VIRTUAL_MEMORY_SDEVICE_STATUS_CHUNK_VALIDATION_FAIL
 } VirtualMemorySDeviceChunkStatus;
+
+#define VIRTUAL_MEMORY_SDEVICE_IS_VALID_CHUNK_STATUS(value) (                                                          \
+{                                                                                                                      \
+   __auto_type _value = (value);                                                                                       \
+   _value == VIRTUAL_MEMORY_SDEVICE_CHUNK_STATUS_OK              ||                                                    \
+   _value == VIRTUAL_MEMORY_SDEVICE_CHUNK_STATUS_PROCESSING_FAIL ||                                                    \
+   _value == VIRTUAL_MEMORY_SDEVICE_CHUNK_STATUS_VALIDATION_FAIL;                                                      \
+})
 
 typedef struct
 {
