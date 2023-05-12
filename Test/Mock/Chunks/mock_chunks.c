3646694adf54112da2a9344792d224f22d2d4c48
Ivan Shestakov <vanyww@mail.ru>
2023-05-12 18:39:00 +0300

    Update tests

--- Test/Mock/Chunks/mock_chunks.c
+++ Test/Mock/Chunks/mock_chunks.c
@@ -8,17 +8,19 @@ typedef struct { size_t ChunkIndex; } MockChunkContext;
 uint8_t MockChunksBuffers[__MOCK_CHUNKS_COUNT][__MOCK_CHUNK_SIZE] = { 0 };
 
 static VirtualMemorySDeviceChunkStatus MockChunkRead(SDEVICE_HANDLE(VirtualMemory) *handle,
+                                                     const VirtualMemorySDeviceChunk *chunk,
                                                      const VirtualMemorySDeviceChunkReadParameters *parameters)
 {
-   size_t chunkIndex = ((const MockChunkContext *)parameters->ChunkContext)->ChunkIndex;
+   size_t chunkIndex = ((const MockChunkContext *)chunk->Context)->ChunkIndex;
    memcpy(parameters->Data, &MockChunksBuffers[chunkIndex][parameters->Offset], parameters->Size);
    return VIRTUAL_MEMORY_SDEVICE_CHUNK_STATUS_OK;
 }
 
 static VirtualMemorySDeviceChunkStatus MockChunkWrite(SDEVICE_HANDLE(VirtualMemory) *handle,
+                                                      const VirtualMemorySDeviceChunk *chunk,
                                                       const VirtualMemorySDeviceChunkWriteParameters *parameters)
 {
-   size_t chunkIndex = ((const MockChunkContext *)parameters->ChunkContext)->ChunkIndex;
+   size_t chunkIndex = ((const MockChunkContext *)chunk->Context)->ChunkIndex;
    memcpy(&MockChunksBuffers[chunkIndex][parameters->Offset], parameters->Data, parameters->Size);
    return VIRTUAL_MEMORY_SDEVICE_CHUNK_STATUS_OK;
 }
