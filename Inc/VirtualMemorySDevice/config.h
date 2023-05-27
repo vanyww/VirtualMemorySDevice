3b25dc4c3f55287a49460bcae58ad02aeb3d3bf0
Ivan Shestakov <vanyww@mail.ru>
2023-05-27 21:23:01 +0300

    Include SDevice core config.h into config.h

--- Inc/VirtualMemorySDevice/config.h
+++ Inc/VirtualMemorySDevice/config.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "SDeviceCore/core.h"
+#include "SDeviceCore/config.h"
 
 #if (SDEVICE_CORE_VERSION_MAJOR != 8) || (SDEVICE_CORE_VERSION_MINOR < 0)
 #error SDevice core version is incorrect.
