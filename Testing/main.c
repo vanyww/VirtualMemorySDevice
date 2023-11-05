10f62115904aec8358056d8d29310a5bc55f43f5
Avvesey Erockhov <kolyann088@gmail.com>
2023-11-05 13:49:13 +0300

    Update 'main.c' file to run updated tests

--- Testing/main.c
+++ Testing/main.c
@@ -1,42 +1,15 @@
-#include "Tests/Initialization/test_initialization.h"
-#include "Tests/Operations/Read/test_read.h"
-#include "Tests/Operations/Write/test_write.h"
+#include "unity_fixture.h"
 
-#include <stdio.h>
-#include <string.h>
 
-struct
+static void RunAllTests(void)
 {
-   bool (*Function)(void);
-   char *Description;
-} TestFunctions[] =
+   RUN_TEST_GROUP(Initialization);
+   RUN_TEST_GROUP(Read);
+   RUN_TEST_GROUP(Write);
+}
+
+int main (int argc, char const *argv[])
 {
-   { TestVirtualMemoryHandleInitialization, "handle initialization" },
-
-   { TestReadNormal, "read normal" },
-   { TestReadEmpty, "read trough empty" },
-
-   { TestWriteNormal, "write normal" },
-   { TestWriteEmpty, "write trough empty" }
-};
-
-const size_t TestFunctionsCount = sizeof(TestFunctions) / sizeof(*TestFunctions);
-
-int main(void)
-{
-   int maxMessageLength = 0;
-   for(size_t i = 0; i < TestFunctionsCount; i++)
-   {
-      int length = strlen(TestFunctions[i].Description);
-      if(length > maxMessageLength)
-         maxMessageLength = length;
-   }
-
-   printf("start tests...\n\n");
-
-   for(size_t i = 0; i < TestFunctionsCount; i++)
-      printf("%*s - %s\n",
-             -maxMessageLength, TestFunctions[i].Description, TestFunctions[i].Function() == true ? "pass" : "fail");
-
-   printf("\nstop tests...\n");
+   UnityMain (argc, argv, RunAllTests);
+   return 0;
 }
